22034f8440a3ce43d634d22f553cebc518194b06
ifeanyi-valentine <urielifeanyi@gmail.com>
2025-12-19 14:54:11 +0300

    style: format template code for consistency and readability

--- toni-cli/src/templates/generate/resource.controller.rs
+++ toni-cli/src/templates/generate/resource.controller.rs
@@ -1,6 +1,6 @@
-use toni_macros::{controller, controller_struct, get, post, put, delete};
-use toni::http_helpers::{HttpRequest, Body};
 use super::resource_name_service::_RESOURCE_NAME_SERVICE;
+use toni::http_helpers::{Body, HttpRequest};
+use toni_macros::{controller, delete, get, post, put};
 
 #[controller(
   "/resource_name",
@@ -9,34 +9,34 @@ use super::resource_name_service::_RESOURCE_NAME_SERVICE;
   }
 )]
 impl _RESOURCE_NAME_CONTROLLER {
-	#[post("")]
-	fn _create(&self, _req: HttpRequest) -> Body {
-		let create: String = self.resource_name_service.create();
-		Body::Text(create)
-	}
+    #[post("")]
+    fn _create(&self, _req: HttpRequest) -> Body {
+        let create: String = self.resource_name_service.create();
+        Body::Text(create)
+    }
 
-	#[get("")]
-	fn _find_all(&self, _req: HttpRequest) -> Body {
-		let find_all: String = self.resource_name_service.find_all();
-		Body::Text(find_all)
-	}
+    #[get("")]
+    fn _find_all(&self, _req: HttpRequest) -> Body {
+        let find_all: String = self.resource_name_service.find_all();
+        Body::Text(find_all)
+    }
 
-	#[get("/{id}")]
-	fn _find_by_id(&self, req: HttpRequest) -> Body {
-		let id = req.path_params.get("id").unwrap().parse::<i32>().unwrap();
-		let find_by_id: String = self.resource_name_service.find_by_id(id);
-		Body::Text(find_by_id)
-	}
+    #[get("/{id}")]
+    fn _find_by_id(&self, req: HttpRequest) -> Body {
+        let id = req.path_params.get("id").unwrap().parse::<i32>().unwrap();
+        let find_by_id: String = self.resource_name_service.find_by_id(id);
+        Body::Text(find_by_id)
+    }
 
-	#[put("")]
-	fn _update(&self, _req: HttpRequest) -> Body {
-		let update: String = self.resource_name_service.update();
-		Body::Text(update)
-	}
+    #[put("")]
+    fn _update(&self, _req: HttpRequest) -> Body {
+        let update: String = self.resource_name_service.update();
+        Body::Text(update)
+    }
 
-	#[delete("")]
-	fn _delete(&self, _req: HttpRequest) -> Body {
-		let delete: String = self.resource_name_service.delete();
-		Body::Text(delete)
-	}
+    #[delete("")]
+    fn _delete(&self, _req: HttpRequest) -> Body {
+        let delete: String = self.resource_name_service.delete();
+        Body::Text(delete)
+    }
 }
--- toni-cli/src/templates/new/src/app/app.controller.rs
+++ toni-cli/src/templates/new/src/app/app.controller.rs
@@ -1,6 +1,6 @@
-use toni_macros::{controller, controller_struct, get, post, put, delete};
-use toni::http_helpers::{HttpRequest, Body};
 use super::app_service::_AppService;
+use toni::http_helpers::{Body, HttpRequest};
+use toni_macros::{controller, delete, get, post, put};
 
 #[controller(
   "/app",
@@ -9,27 +9,27 @@ use super::app_service::_AppService;
   }
 )]
 impl _AppController {
-	#[post("")]
-	fn _create(&self, _req: HttpRequest) -> Body {
-		let create: String = self.app_service.create();
-		Body::Text(create)
-	}
+    #[post("")]
+    fn _create(&self, _req: HttpRequest) -> Body {
+        let create: String = self.app_service.create();
+        Body::Text(create)
+    }
 
-	#[get("")]
-	fn _find_all(&self, _req: HttpRequest) -> Body {
-		let find_all: String = self.app_service.find_all();
-		Body::Text(find_all)
-	}
+    #[get("")]
+    fn _find_all(&self, _req: HttpRequest) -> Body {
+        let find_all: String = self.app_service.find_all();
+        Body::Text(find_all)
+    }
 
-	#[put("")]
-	fn _update(&self, _req: HttpRequest) -> Body {
-		let update: String = self.app_service.update();
-		Body::Text(update)
-	}
+    #[put("")]
+    fn _update(&self, _req: HttpRequest) -> Body {
+        let update: String = self.app_service.update();
+        Body::Text(update)
+    }
 
-	#[delete("")]
-	fn _delete(&self, _req: HttpRequest) -> Body {
-		let delete: String = self.app_service.delete();
-		Body::Text(delete)
-	}
+    #[delete("")]
+    fn _delete(&self, _req: HttpRequest) -> Body {
+        let delete: String = self.app_service.delete();
+        Body::Text(delete)
+    }
 }
